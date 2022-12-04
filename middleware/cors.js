2287403bfaace56fd209b2b50f63f7fd4c710ba4
harshitScript <85029089+harshitScript@users.noreply.github.com>
2022-12-04 16:14:44 +0530

    cors quick fix

--- middleware/cors.js
+++ middleware/cors.js
@@ -2,9 +2,10 @@ const cors = (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET POST PATCH DELETE PUT OPTIONS"
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 };
+
 module.exports = cors;
