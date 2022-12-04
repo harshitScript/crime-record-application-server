d08ce78e60c0c0fa24cf9c05d254a43fd614b79a
harshitScript <85029089+harshitScript@users.noreply.github.com>
2022-12-04 16:08:54 +0530

    cors fixed

--- middleware/cors.js
+++ middleware/cors.js
@@ -2,7 +2,7 @@ const cors = (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET POST PATCH DELETE PUT OPTION"
+    "GET POST PATCH DELETE PUT OPTIONS"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
