004cff3b8f69ec40ae190705967ead2ab4c90037
harshitScript <85029089+harshitScript@users.noreply.github.com>
2023-01-17 22:03:02 +0530

    cookies implemented --test

--- controllers/visitor/trackVisitorController.js
+++ controllers/visitor/trackVisitorController.js
@@ -1,12 +1,8 @@
 const { generateHash } = require("../../utils/helper");
 const trackVisitorController = (req, res) => {
-  console.log("The request cookies => ", req.cookies);
   res.cookie(
     "v.id",
-    generateHash({
-      string: Math.random().toString(),
-      secret: process.env.VISITOR_SECRET,
-    }),
+    "harshit",
     { secure: true, httpOnly: true }
   );
   res.status(201).json({
