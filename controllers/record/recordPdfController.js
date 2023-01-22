c42530fef05b0e78285e3750a4d6915d1dec4c85
harshitScript <85029089+harshitScript@users.noreply.github.com>
2023-01-22 20:55:48 +0530

    change in dependencies --2

--- controllers/record/recordPdfController.js
+++ controllers/record/recordPdfController.js
@@ -1,8 +1,4 @@
 const Record = require("../../models/record");
-const pdf = require("html-pdf");
-const prepareRecordPdfHTML = require("../../PDF-HTML/record-pdf");
-const path = require("path");
-const rootDir = require("../../utils/rootDir");
 
 const recordPdfController = async (req, res, next) => {
   const { recordId } = req.params;
@@ -14,37 +10,9 @@ const recordPdfController = async (req, res, next) => {
       throw error;
     }
 
-    let options = {
-      format: "A4",
-      orientation: "portrait",
-      phantomPath: path.join(
-        rootDir,
-        "node_modules",
-        "phantomjs-prebuilt",
-        "bin",
-        "phantomjs"
-      ),
-      border: {
-        top: "1cm", // default is 0, units: mm, cm, in, px
-        right: "2cm",
-        bottom: "1cm",
-        left: "1cm",
-      },
-    };
-
-    pdf
-      .create(prepareRecordPdfHTML(record, options))
-      .toStream(function (err, pdfStream) {
-        if (err) {
-          throw err;
-        }
-        res.setHeader("Content-Type", "application/pdf");
-        res.setHeader("Content-Disposition", "inline;filename=test.pdf");
-        pdfStream.on("end", () => {
-          res.end();
-        });
-        pdfStream.pipe(res);
-      });
+    res.json({
+      message: "soon this service will be enabled.",
+    });
 
     return 1;
   } catch (error) {
