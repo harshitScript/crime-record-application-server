af79026419f65f3b1fc250cfca59f24439bad2e0
harshitScript <85029089+harshitScript@users.noreply.github.com>
2023-01-22 22:22:04 +0530

    new pdf generation logic implemented

--- controllers/record/recordPdfController.js
+++ controllers/record/recordPdfController.js
@@ -1,4 +1,9 @@
 const Record = require("../../models/record");
+const path = require("path");
+const rootDir = require("../../utils/rootDir");
+const prepareRecordPdfHTML = require("../../PDF-HTML/record-pdf");
+const { pdfUtils } = require("../../utils/helper");
+const fs = require("fs");
 
 const recordPdfController = async (req, res, next) => {
   const { recordId } = req.params;
@@ -10,8 +15,26 @@ const recordPdfController = async (req, res, next) => {
       throw error;
     }
 
-    res.json({
-      message: "soon this service will be enabled.",
+    //* PDF FUNCTIONALITY
+    const pdfPath = path.join(
+      rootDir,
+      "assets",
+      "pdf",
+      `record_${record?._id}.pdf`
+    );
+    const pdfContent = prepareRecordPdfHTML(record);
+    await pdfUtils.generate({ path: pdfPath, htmlString: pdfContent });
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader(
+      "Content-Disposition",
+      `inline:filename=record_${record?._id}.pdf`
+    );
+    const pdfStream = fs.createReadStream(pdfPath);
+    pdfStream.pipe(res);
+    pdfStream.on("end", () => {
+      //? will delete the pdf in background.
+      res.end();
+      pdfUtils.delete({ path: pdfPath });
     });
 
     return 1;
--- utils/helper.js
+++ utils/helper.js
@@ -1,4 +1,6 @@
 const { createHmac } = require("crypto");
+const puppeteer = require("puppeteer");
+const fs = require("fs/promises");
 
 const generateExpiryInMilSeconds = ({ hours = 1 }) => {
   const currentDate = new Date();
@@ -12,7 +14,44 @@ const generateHash = ({ algorithm = "sha512", string = "", secret = "" }) => {
   return createHmac(algorithm, secret).update(string).digest("hex");
 };
 
+const pdfUtils = {
+  generate: async ({ path = "", htmlString = "" }) => {
+    //* PDF GENERATION CODE. ----------------------------------------------------
+    try {
+      //? launches puppeteer in the browser.
+      const browser = await puppeteer.launch();
+      //? Opens a new page.
+      const page = await browser.newPage();
+      //? Sets content for the page
+      await page.setContent(htmlString);
+      //? Creates a pdf document
+      await page.pdf({
+        path: path,
+        format: "A4",
+        printBackground: true,
+      });
+      //? closes the puppeteer in the browser
+      await browser.close();
+
+      return 1;
+    } catch {
+      return 0;
+    }
+
+    //* ---------------------------------------------------------------------------------
+  },
+  delete: async ({ path = "" }) => {
+    try {
+      await fs.unlink(path);
+      return 1;
+    } catch (error) {
+      return 0;
+    }
+  },
+};
+
 module.exports = {
   generateExpiryInMilSeconds,
   generateHash,
+  pdfUtils,
 };
